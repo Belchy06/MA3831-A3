a0b3d4ea98502f56dfc3559d88fdc830c7fbe551
Belchy06 <william.belcher@hotmail.com>
2021-04-19 08:44:41 +1000

    Initial files - get job categories and sub-categories

---
+++ search_setting.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    'Classification': 'jobs-in-accounting',  # for IT jobs
+    # 'Classification' : 'jobs-in-accounting',  # for Accounting jobs
+
+    'Where': '',
+    # 'Where' : 'in-All-Sydney-NSW',
+
+    'Daterange': '7',
+    # 'Daterange' : '14',
+    # 'Daterange' : '31',
+}
